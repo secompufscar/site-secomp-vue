2ae963a0f15b4b8a425ce34724be54965ff28f3b
Pabolo Pires <pabolo18@gmail.com>
2020-10-11 22:16:23 -0300

    Add active class to current route on NavBar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,30 +1,24 @@
 <template>
-    <b-navbar class="shadow-sm bg-gradient-secomp text-navbar-secomp" fixed="top" type="dark" toggleable="lg">
-      <b-navbar-brand :to="{name: 'Home'}" href="#">
-        <img id="logo" src="../assets/logo.png" alt="Logo da SECOMP UFSCar"/>
-      </b-navbar-brand>
+  <b-navbar class="shadow-sm bg-gradient-secomp" fixed="top" type="dark" toggleable="lg">
+    <b-navbar-brand :to="{name: 'Home'}" href="#">
+      <img id="logo" src="../assets/logo.png" alt="Logo da SECOMP UFSCar"/>
+    </b-navbar-brand>
 
-      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
-          <b-nav-item :to="{ name: 'Home' }">
-            <font-awesome-icon icon="home"></font-awesome-icon>
-            Home
-          </b-nav-item>
-          <b-nav-item :to="{ name: 'Events' }">
-            <font-awesome-icon icon="calendar-alt"></font-awesome-icon>
-            Eventos
-          </b-nav-item>
-
-        </b-navbar-nav>
-
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-
-        </b-navbar-nav>
-      </b-collapse>
-    </b-navbar>
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item exact-active-class="active" :to="{ name: 'Home' }">
+          <font-awesome-icon icon="home"></font-awesome-icon>
+          Home
+        </b-nav-item>
+        <b-nav-item active-class="active" :to="{ name: 'Events' }">
+          <font-awesome-icon icon="calendar-alt"></font-awesome-icon>
+          Eventos
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
 </template>
 
 <style>
@@ -35,9 +29,5 @@
 
 .bg-gradient-secomp {
   background-image: linear-gradient(to right, #008cb5, #00cfa1);
-}
-
-.text-navbar-secomp a {
-  color: white !important;
 }
 </style>
